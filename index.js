46f58f5a18b05d9ece5ef35bd2a17cba949a53ce
Thosine-01 <thosine7900>
2025-01-31 21:53:48 +0100

    first commit for HNG task

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define the data to be returned
+const data = {
+  email: 'lawaltosine01@gmail.com',
+  datetime: new Date().toISOString(),
+  github_repo_url: 'https://github.com/Thosine-01/HNG_Stage_0', 
+};
+
+// Define the API endpoint
+app.get('/api', (req, res) => {
+  res.json(data);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
